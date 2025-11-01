f05fcb37392ec2410b8d36e0af1a8db3bd0403b8
Mohamed Ahmed <es.Mohamed.nomier2023@alexu.edu.eg>
2025-11-01 15:22:12 +0200

    Added the Project Structure

---
+++ backend/src/main/java/com/hospital/medalert/MedalertApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.medalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedalertApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hospital/medalert/MedalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.medalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
