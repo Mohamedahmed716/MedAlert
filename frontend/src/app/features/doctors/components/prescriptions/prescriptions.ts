9cd37652f32292d74d8f12f4241acb73c991de61
ali <aliamr582005@gmail.com>
2025-12-01 14:06:11 +0200

    finished reservation, prescr. , dashboard, patients, and pop up feedback

--- frontend/src/app/features/doctors/components/prescriptions/prescriptions.ts
+++ frontend/src/app/features/doctors/components/prescriptions/prescriptions.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './prescriptions.css',
 })
 export class Prescriptions {
+  prescriptions = [
+    {patient: 'John Doe', date: '2023-10-24', medication: 'Drug A', dosage: '10mg', frequency: 'Once a day', duration: '30 days'},
+    {patient: 'Jane Smith', date: '2023-10-24', medication: 'Drug B', dosage: '5mg', frequency: 'Twice a day', duration: '14 days'},
+    {patient: 'Alice Johnson', date: '2023-10-24', medication: 'Drug C', dosage: '20mg', frequency: 'Once a day', duration: '60 days'},
+  ];
 
+  showNotification = false;
+
+  sendPrescription() {
+    this.showNotification = true;
+
+    setTimeout(() => {
+      this.showNotification = false;
+    }, 2000);
+  }
 }
+
--- frontend/src/app/features/doctors/components/reservations/reservations.ts
+++ frontend/src/app/features/doctors/components/reservations/reservations.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-reservations',
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './reservations.html',
   styleUrl: './reservations.css',
 })
 export class Reservations {
+  currentDate: Date = new Date();
+  shift = ["9:00AM" , "5:00PM"]; // Example shift times
+
+  // Sample reservation data
+  todayReservations = [
+    { patient: 'Jane Doe', time: '9:00 AM', condition: 'Check-up', status: 'Confirmed'},
+    { patient: 'Bob Smith', time: '10:00 AM', condition: 'Flu', status: 'Cancelled'}
+  ];
+  res_num = this.todayReservations.length;
+
+  allReservations = [
+    { patient: 'John Doe', date: 'Nov 16', time: '9:00 AM', condition: 'Flu', status: 'Confirmed' },
+    { patient: 'Jane Smith', date: 'Nov 15', time: '10:00 AM', condition: 'Check-up', status: 'Cancelled' }
+  ];
 
 }
