fe73366b5748dbdb58afff98b1349af34aec9ae1
ali <aliamr582005@gmail.com>
2025-12-14 15:04:27 +0200

    added some services

---
+++ frontend/src/app/features/doctors/services/patient-service.ts
@@ -0,0 +1,16 @@
+import {inject, Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Patient} from '../../../shared/ui/models/patient';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PatientService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/api/doctor';
+
+  getRecentPatients(): Observable<Patient[]> {
+    return this.http.get<Patient[]>(`${this.apiUrl}/patients/recent`);
+  }
+}
--- frontend/src/app/shared/ui/models/reservation.ts
+++ frontend/src/app/shared/ui/models/reservation.ts
@@ -1,11 +1,8 @@
-import { Patient } from './patient';
-import { Doctor } from './doctor';
 import { ReservationStatus } from './enums';
 
 export interface Reservation {
   id: number;
-  doctor: Doctor;
-  patient: Patient;
+  patientName: string;
   appointmentTime: string; // ISO DateTime
   reason: string;
   status: ReservationStatus;
